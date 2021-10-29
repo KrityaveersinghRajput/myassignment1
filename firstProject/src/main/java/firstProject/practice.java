e6ae6df8d31656e18fa88e2b42705ea8a47a1b2a
Krityaveer <singhkrityaveer@gmail.com>
2021-10-29 11:37:42 +0530

    First commit Uploading All Files

---
+++ firstProject/src/main/java/firstProject/practice.java
@@ -0,0 +1,23 @@
+package firstProject;
+
+public class practice
+{
+public static void main (String []args)
+{
+	byte age=29;
+	short salary=30000;
+	int pincode=844101;
+	long phone=912833989;
+	float percentage=64.4f;
+	double no= 99999999.5855;
+	boolean isIndian=true;
+	char section = 'A';
+	System.out.println(age);
+	System.out.println(salary);
+	System.out.println(pincode);
+	System.out.println(phone);
+	System.out.println(percentage);
+	System.out.println(no);
+	System.out.println(isIndian);
+}
+}
